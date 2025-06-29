b30398d5e30feeaac1570eac0e0428d7841a9546
lunarsheep <dylenyedc@foxmail.com>
2025-06-29 12:49:21 +0800

    6.29update

--- fileop/scoresheet.py
+++ fileop/scoresheet.py
@@ -1,1 +1,11 @@
-from openpyxl import Workbook
+from openpyxl import load_workbook
+
+workBook=load_workbook("fileop/ScoresSheet.xlsx")
+workSheet=workBook.worksheets[0]
+for idx, row in enumerate(workSheet.rows):
+    if idx == 0:
+        continue
+    row[5].value =int(row[1].value) * 0.3 + int(row[2].value ) * 0.1 \
+        + int(row[3].value ) * 0.2 + int(row[4].value) * 0.4
+
+workBook.save("fileop/ScoresSheet2.xlsx")
